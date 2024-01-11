a935c6c28cfc3ca1c9d64f5c35217ac1967946ab
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-11 18:26:03 -0500

    Anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,30 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+
+        def get_letters_list(word):
+            letters = []
+            for letter in word:
+                letters.append(letter)
+            return letters
+        
+        matching_words = []
+
+        for word in word_list:
+            if sorted(get_letters_list(self.word)) == sorted(get_letters_list(word)):
+                matching_words.append(word)
+            elif len(word_list) == 0:
+                return []
+            else:
+                pass
+            
+        return matching_words
+            
+        
+
+
+anagram = Anagram("listen")
+print(anagram.match(["enlist", "sausage"]))
+
